5e46560130c50b9d4e00c31b93a7c880b997c7c0
jneyens1 <jari.neyens@telenetgroup.be>
2022-08-03 08:22:59 +0200

    moved to multi thread

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,15 +2,21 @@ use std::{
     fs,
     io::prelude::*,
     net::{TcpListener, TcpStream},
+    thread::{self},
+    time::Duration,
 };
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_coonnection(stream);
+        pool.execute(|| {
+            handle_coonnection(stream);
+        });
     }
 }
 
@@ -20,7 +26,12 @@ fn handle_coonnection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
